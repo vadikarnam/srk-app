74d8859423fe88b8ab4d59e44788bae4d98eecaf
vkaranam7 <vkaranam7@dxc.com>
2020-12-10 15:02:24 +0530

    basic project structure

---
+++ srk-app/src/main/java/com/vkaranam/spring/SrkAppApplication.java
@@ -0,0 +1,13 @@
+package com.vkaranam.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrkAppApplication.class, args);
+	}
+
+}
---
+++ srk-app/src/test/java/com/vkaranam/spring/SrkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkaranam.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
